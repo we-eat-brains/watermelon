5ed1e81cf18f24484a164046a2a0f5f464e26e44
Dario Ghilardi <darioghilardi@webrain.it>
2016-03-24 18:15:47 +0100

    Configure babel with webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,16 @@ module.exports = {
   entry: [
     './src/index.js'
   ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: BUILD_PATH,
     publicPath: '/',
