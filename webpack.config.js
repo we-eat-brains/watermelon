25dd64c63ec66e67ff21829ca354da138161b5da
Dario Ghilardi <darioghilardi@webrain.it>
2016-03-25 11:32:33 +0100

    Configure the react-hot-loader plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'src');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var APP_HOST = 'edev'
 
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://' + APP_HOST + ':8080',
+    'webpack/hot/only-dev-server',
     './src/index.js'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'react-hot!babel'
     }]
   },
   resolve: {
@@ -25,7 +29,11 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: BUILD_PATH
+    contentBase: BUILD_PATH,
+    hot: true
   },
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
